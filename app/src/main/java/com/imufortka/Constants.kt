cb670e1963b7176f9b7505ab9d48fca081286d54
Sathish-Kumar-Kanagaraj <sathishkumark748@gmail.com>
2021-07-05 12:39:08 +0530

    constant changes

--- app/src/main/java/com/imufortka/Constants.kt
+++ app/src/main/java/com/imufortka/Constants.kt
@@ -20,7 +20,7 @@ class Constants {
      /*   val SERVICE_UUID1:UUID=UUID.fromString(App.getStringPrefernce(BARCODE1,"00001523-1212-efde-1523-785feabcd123"))
         val SERVICE_UUID2:UUID=UUID.fromString(App.getStringPrefernce(BARCODE2,"0000b81d-0000-1000-8000-00805f9b34fb"))*/
 
-        val MESSAGE_UUID: UUID = UUID.fromString("00001525-1212-efde-1523-785feabcd123")
+        val MESSAGE_UUID: UUID = UUID.fromString("00001528-1212-efde-1523-785feabcd123")
 
         /**
          * UUID to confirm device connection
